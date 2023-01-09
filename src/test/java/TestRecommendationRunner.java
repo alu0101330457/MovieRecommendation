aea5d31cc5c660be3637f8b813f1a873cb733478
alu0101330457 <72342734+alu0101330457@users.noreply.github.com>
2023-01-09 11:58:12 +0000

    Commit
    Actualización de la versión del plugin de seguridad.
    Arreglo de las dependencias de las clases dedicadas a los tests.

--- src/test/java/TestRecommendationRunner.java
+++ src/test/java/TestRecommendationRunner.java
@@ -1,21 +1,19 @@
-package movierecommendation.recommenderrunner;
+import movierecommendation.recommenderrunner.RecommendationRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-
 import java.util.ArrayList;
 
-class RecommendationRunnerTest {
-
+public class TestRecommendationRunner {
 
     @DisplayName("Test if we can get the movies to rate")
     @Nested
-    class RecommendationRunnerTest0 {
+    public class Test0RecommendationRunner {
         @DisplayName("Test if we can get the movies to rate")
         @org.junit.jupiter.api.Test
-        void test() {
+        public void test() {
             RecommendationRunner recommendationRunner = new RecommendationRunner();
             recommendationRunner.getItemsToRate();
         }
@@ -23,10 +21,10 @@ class RecommendationRunnerTest {
 
     @DisplayName("When we call the method getItemsToRate it should return a list of movies")
     @Nested
-    class RecommendationRunnerTest1 {
+   public class Test1RecommendationRunner {
         @DisplayName("When we call the method getItemsToRate it should return a list of movies")
         @org.junit.jupiter.api.Test
-        void test() {
+        public void test() {
             RecommendationRunner recommendationRunner = new RecommendationRunner();
             ArrayList<String> films = recommendationRunner.getItemsToRate();
             // Comprobar si films tiene 10 elementos
@@ -41,38 +39,38 @@ class RecommendationRunnerTest {
     @DisplayName("The method printOut is working")
     @Nested
 
-    class RecommendationRunnerTest2 {
+    public class Test2RecommendationRunner {
         @DisplayName("The method printOut is working")
         @org.junit.jupiter.api.Test
-        void test() {
+        public void test() {
             RecommendationRunner recommendationRunner = new RecommendationRunner();
             recommendationRunner.printOut("0", 0);
         }
     }
-    
+
     @DisplayName("The method printOut returns a String")
     @Nested
-    class RecommendationRunnerTest3 {
+    public class Test3RecommendationRunner {
         @DisplayName("The method printOut returns a String")
         @org.junit.jupiter.api.Test
-        void test() {
+        public void test() {
             RecommendationRunner recommendationRunner = new RecommendationRunner();
             String result = recommendationRunner.printOut("0", 0);
             assertTrue(result instanceof String);
         }
-    }   
+    }
 
     @DisplayName("We cant pass a negative number to printRecommendationsFor method")
     @Nested
-    class RecommendationRunnerTest4 {
+    public class Test4RecommendationRunner{
         @DisplayName("We cant pass a negative number to printRecommendationsFor method")
         @org.junit.jupiter.api.Test
-        void test() {
+        public void test() {
             RecommendationRunner recommendationRunner = new RecommendationRunner();
             assertThrows(NullPointerException.class, () -> {
                 recommendationRunner.printRecommendationsFor("-1");
             });
         }
     }
-  
+
 }
